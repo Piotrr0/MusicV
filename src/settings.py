de5f0d6416c02ab165be5d423b1d3ef9b53ab9ab
Piotrr0 <piotrr.nawalany@gmail.com>
2025-06-02 21:02:29 +0200

    Settings for downloading an audio

--- src/settings.py
+++ src/settings.py
@@ -3,3 +3,15 @@ class Settings():
     is_dragging_volume = False
     is_playing = False
     fft_window_size = 4096
+
+    download_audio_qualities = {
+        'high-quality': 'bestaudio/best',
+        'low-quality': 'worstaudio/worst'
+    }
+
+    download_audio_quality  = download_audio_qualities['high-quality']
+
+    download_audio_bitrate = 192
+
+    download_audio_ext = 'mp3'
+    download_output_dir = 'audio'
--- src/youtubeHandler.py
+++ src/youtubeHandler.py
@@ -1,21 +1,21 @@
 import yt_dlp
 import os
-import re
+from settings import Settings
 
 class YoutubeHandler():
     def __init__(self):
-        self.output_dir = os.path.join(os.getcwd(), "audio")
+        self.output_dir = os.path.join(os.getcwd(), Settings.download_output_dir)
 
     def get_audio_from_youtube(self, video_url: str) -> str:
         os.makedirs(self.output_dir, exist_ok=True)
 
         ydl_opts = {
-            "format": "bestaudio/best",
+            "format": Settings.download_audio_quality,
             "outtmpl": os.path.join(self.output_dir, "%(title)s.%(ext)s"),
             "postprocessors": [{
                 "key": "FFmpegExtractAudio",
-                "preferredcodec": "wav",
-                "preferredquality": "192",
+                "preferredcodec": Settings.download_audio_ext,
+                "preferredquality": Settings.download_audio_bitrate,
             }],
             "keepvideo": False,
             "quiet": True,
@@ -27,5 +27,5 @@ class YoutubeHandler():
             title = info_dict.get("title", "audio")
 
         for file in os.listdir(self.output_dir):
-            if file.endswith(".wav") and title in file:
+            if file.endswith('.' + Settings.download_audio_ext) and title in file:
                 return os.path.join(self.output_dir, file)
